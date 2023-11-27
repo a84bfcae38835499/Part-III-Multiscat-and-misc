aef2880e30e1d6695f02a4841e9efbc336eeefa6
UqUHXBs2VeA <oap22@cam.ac.uk>
2023-11-27 16:46:13 +0000

    kuhkjbhkjjklklj

--- create_potential.m
+++ create_potential.m
@@ -84,8 +84,8 @@ for k = 1:size(V,3) %Should be the z direction
     for j = 1:1:gridp*how_many_cell
       x = X(i,j);
       y = Y(i,j);
-        val = -5*Gaussian2D(x,y, ...
-            [const.a*1,const.a*1],const.a/5,3*const.D*dropoff)/2;
+        val = -2*Gaussian2D(x,y, ...
+            [const.a*1.15,const.a*1.25],const.a/5,3*const.D*dropoff)/2;
       V(i,j,k) = V(i,j,k)+val;
       disp("x, y, z = " + x + ", " + y + ", " + z(k) +...
           ", Value = " + val);
