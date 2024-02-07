8656fd25e3caa11a48aaf18889114a6e6f28a998
Realviv <oap22@cam.ac.uk>
2024-02-07 19:12:32 +0000

    New diffraction figureA

--- matplotlib-pyplot-hexbin-1.py
+++ matplotlib-pyplot-hexbin-1.py
@@ -27,7 +27,6 @@ def calculate_entropy(intensities):
     H /= -np.log(intensities.size)
     return(H)
 
-
 latticeFile = open('latticeVects.info_for_vivian_python_nice_plotting_hexagon_script', 'r')
 count = 0
 
@@ -36,8 +35,8 @@ B2 = [0,0]
 while count < 2:
     # Get next line from file
     line = latticeFile.readline()
-    if line.startswith("B1 = "):
-        line = line[len("B1 = "):]
+    if line.startswith("b1 = "):
+        line = line[len("b1 = "):]
         print("line = " + line)
         split = line.split()
 
@@ -45,8 +44,8 @@ while count < 2:
         B1[1] = float(split[1])
         count += 1
  
-    if line.startswith("B2 = "):
-        line = line[len("B2 = "):]
+    if line.startswith("b2 = "):
+        line = line[len("b2 = "):]
         print("line = " + line)
         split = line.split()
         B2[0] = float(split[0])
@@ -54,9 +53,9 @@ while count < 2:
         count += 1
 
 Babs = np.sqrt(B1[0]**2+B1[1]**2)
-print("B1 = ") 
+print("b1 = ") 
 print(B1)
-print("B2 = ") 
+print("b2 = ") 
 print(B2)
 print("Babs = ") 
 print(Babs)
@@ -73,6 +72,8 @@ plotCoordsX = np.zeros((nOccCh))
 plotCoordsY = np.zeros((nOccCh))
 nmin = 0
 nmax = 0
+valmin = 1
+valmax = 0
 for k in range(0,nOccCh):
     row = d.iloc[k]
     n1 = getattr(row,'n1')
@@ -82,6 +83,10 @@ for k in range(0,nOccCh):
         nmax = n1
     n2 = getattr(row,'n2')
     I = getattr(row,'I')
+    if(I < valmin):
+        valmin = I
+    if(I > valmax):
+        valmax = I
     #print(f"k = {k}, n1 = {n1}, n2 = {n2}, I = {I}")
     plotValues[k] = I
     plotCoordsY[k] = -B1[0] * n1 - B2[0] * n2 #for some reason everything gets inverted?? and x and y are swapped from what I'd expect?????
@@ -99,12 +104,12 @@ fig, ax = plt.subplots(figsize=(4, 4))
 #print(plotCoordsY)
 
 xSpan = nmax-nmin
-xSpan = xSpan
+xSpan = xSpan*0.5
 print(xSpan)
 
-h = ax.hexbin(plotCoordsX/Babs,plotCoordsY/Babs,C=plotValues,gridsize=(int(np.sqrt(3)*(xSpan)), int(xSpan)),cmap='magma',bins='log',norm=mpl.colors.Normalize(0,1))
+h = ax.hexbin(plotCoordsX/Babs,plotCoordsY/Babs,C=plotValues,gridsize=(int(np.sqrt(3)*(xSpan)), int(xSpan)),cmap='magma',bins='log',norm=mpl.colors.Normalize(valmin,valmax))
  
-fig.colorbar(mpl.cm.ScalarMappable(norm=mpl.colors.LogNorm(vmin=1e-5,vmax=1), cmap='magma'),
+fig.colorbar(mpl.cm.ScalarMappable(norm=mpl.colors.LogNorm(vmin=valmin,vmax=valmax), cmap='magma'),
              ax=ax, orientation='vertical', label='P($n_1$,$n_2$)')
 
 savestr = "Figures/Diffraction/" + datetime.datetime.now().strftime('Diffraction_%Y-%m-%d_%H-%M') + "_Hex.png"
