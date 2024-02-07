e441766b56b0996c330114f10efe406c81296547
UqUHXBs2VeA <oap22@cam.ac.uk>
2024-02-07 19:23:17 +0000

    ngnfgnfngfnfgn

--- potential_maker.m
+++ potential_maker.m
@@ -3,7 +3,7 @@ rng default;
 
 %Number of grid points, number of Z points, and number of lattices
 %contained in the overall superlattice (or rather the square root of that)
-Nxy = 64; Nz = 150; Nsuper = 1;
+Nxy = 128; Nz = 150; Nsuper = 1;
 zMax = 6; zMin = -2;%units Å
 
 %a = 2.84Å. see const.m for more stuff
