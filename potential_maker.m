43157a6f06424674cda65a52f34ea114f47f9a42
UqUHXBs2VeA <oap22@cam.ac.uk>
2024-02-15 16:09:45 +0000

    made potentials worse on purppse

--- potential_maker.m
+++ potential_maker.m
@@ -3,7 +3,7 @@ rng default;
 
 %Number of grid points, number of Z points, and number of lattices
 %contained in the overall superlattice (or rather the square root of that)
-Nxy = 120; Nz = 190; Nsuper = 1;
+Nxy = 12; Nz = 19; Nsuper = 1;
 zMax = 6; zMin = -1;%units Å
 
 %a = 2.84Å. see const.m for more stuff
