a3db27d1c063306bcb3d3f0b031f36465350d926
UqUHXBs2VeA <oap22@cam.ac.uk>
2024-02-18 17:21:53 +0000

    nearest supercell

--- potential_maker.m
+++ potential_maker.m
@@ -3,7 +3,7 @@ rng default;
 
 %Number of grid points, number of Z points, and number of lattices
 %contained in the overall superlattice (or rather the square root of that)
-Nxy = 64; Nz = 50; Nsuper = 1;
+Nxy = 64; Nz = 50; Nsuper = 2;
 zMax = 6; zMin = 1.5;%units Å
 
 %a = 2.84Å. see const.m for more stuff
