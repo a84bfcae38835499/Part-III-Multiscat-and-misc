91a29808c4046430bc327d90ad06c4e5e2f6ff3d
Realviv <oap22@cam.ac.uk>
2024-02-19 11:02:25 +0000

    New diffraction figure

--- matplotlib-pyplot-hexbin-1.py
+++ matplotlib-pyplot-hexbin-1.py
@@ -214,7 +214,7 @@ def slugify(value, allow_unicode=False):
     return re.sub(r'[-\s]+', '-', value).strip('-_')
 
 filenametxt=""
-filenametxt="Naturally interpolated DFT"
+filenametxt="Nxy = 64"
 plt.figtext(0.5, 0.01, filenametxt, wrap=True, horizontalalignment='center', fontsize=12,fontstyle='italic')
 
 if(filenametxt == ""):
