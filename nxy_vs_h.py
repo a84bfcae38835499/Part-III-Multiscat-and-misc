ca24ed783c6e080013e184010a19143667022395
Realviv <oap22@cam.ac.uk>
2024-02-19 11:31:06 +0000

    New diffraction figure

--- nxy_vs_h.py
+++ nxy_vs_h.py
@@ -3,4 +3,12 @@ from matplotlib import pyplot as plt
 Nxys = [64, 32, 16, 8, 4]
 Entropies = [0.850912701202775, 0.850912701202775,0.850912701202775, 0.850912701202775, 0.850912701202775]
 
-plt.plot(Nxys,Entropies)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(Nxys,Entropies,linestyle="-",marker='o',c=[0,0,0])
+plt.title("Entropy vs Nxy")
+plt.xlim(0,64)
+plt.xlabel("Nxy")
+plt.ylim(0,1)
+plt.ylabel("Entropy")
+plt.show()
