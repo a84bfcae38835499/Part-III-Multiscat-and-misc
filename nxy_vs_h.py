71579d2bdd88cc24dd5507b96d13a1a7463a19db
UqUHXBs2VeA <oap22@cam.ac.uk>
2024-02-19 11:25:38 +0000

    egsegs

--- nxy_vs_h.py
+++ nxy_vs_h.py
@@ -1,3 +1,6 @@
-Nxys = [64, 32, 16, 8, 4]
+from matplotlib import pyplot as plt
 
+Nxys = [64, 32, 16, 8, 4]
 Entropies = [0.850912701202775, 0.850912701202775,0.850912701202775, 0.850912701202775, 0.850912701202775]
+
+plt.plot(Nxys,Entropies)
