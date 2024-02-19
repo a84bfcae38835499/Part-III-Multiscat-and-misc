192e35c8239b3ab1731012b8d7d09e57193e6c8d
Realviv <oap22@cam.ac.uk>
2024-02-19 11:03:03 +0000

    New diffraction figure

--- matplotlib-pyplot-hexbin-1.py
+++ matplotlib-pyplot-hexbin-1.py
@@ -214,7 +214,7 @@ def slugify(value, allow_unicode=False):
     return re.sub(r'[-\s]+', '-', value).strip('-_')
 
 filenametxt=""
-filenametxt="Nxy = 64"
+filenametxt="Nxy = 32"
 plt.figtext(0.5, 0.01, filenametxt, wrap=True, horizontalalignment='center', fontsize=12,fontstyle='italic')
 
 if(filenametxt == ""):
