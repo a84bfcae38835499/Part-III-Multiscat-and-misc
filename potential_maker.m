b1740d89192902a65494f6927ad89a9259eede85
UqUHXBs2VeA <oap22@cam.ac.uk>
2024-03-08 22:11:19 +0000

    ,mm,

--- potential_maker.m
+++ potential_maker.m
@@ -4,9 +4,9 @@ rng("shuffle");
 
 %Number of grid points, number of Z points, and number of lattices
 %contained in the overall superlattice (or rather the square root of that)
-Nxy = 32; Nz = 50; Nsuper = 4;
-%Theta = 0.1;
-Theta = (0/(Nsuper*Nsuper));
+Nxy = 32; Nz = 50; Nsuper = 3;
+Theta = 0.2;
+%Theta = (/(Nsuper*Nsuper));
 disp('Theta = ' + Theta)
 zMax = 6; zMin = 0;%units Å
 
@@ -266,7 +266,7 @@ else
   Nensemble = 1;
 end
 disp("Total ensemble size = " + Nensemble)
-Nensemble_limit = Nsuper; %This is a very very rough lower bound
+Nensemble_limit = 10; %This is a very very rough lower bound
 if(Nensemble > Nensemble_limit)
   disp("Truncating ensemble to just " + Nensemble_limit)
   Nensemble = Nensemble_limit;
