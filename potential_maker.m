19df8bc7787ea91492eb582d431864391b8d5b32
UqUHXBs2VeA <oap22@cam.ac.uk>
2024-03-09 12:23:16 +0000

    asfasf

--- potential_maker.m
+++ potential_maker.m
@@ -8,7 +8,7 @@ Nxy = 32; Nz = 100; Nsuper = 1;
 Theta = 0.0;
 %Theta = (/(Nsuper*Nsuper));
 disp('Theta = ' + Theta)
-zMax = 0.1; zMin = 0;%units Å
+zMax = 2; zMin = 0;%units Å
 
 %a1=[const.a,0];
 %a2=[0,const.a]; 
