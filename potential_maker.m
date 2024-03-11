ea92dc6c07512dd781a17674b3349228c6f9bb46
UqUHXBs2VeA <oap22@cam.ac.uk>
2024-03-11 08:02:07 +0000

    smaller cell

--- potential_maker.m
+++ potential_maker.m
@@ -4,9 +4,9 @@ rng("shuffle");
 
 %Number of grid points, number of Z points, and number of lattices
 %contained in the overall superlattice (or rather the square root of that)
-Nxy = 32; Nz = 60; Nsuper = 3;
+Nxy = 32; Nz = 60; Nsuper = 2;
 %Theta = 0.0;
-Theta = (3/(Nsuper*Nsuper));
+Theta = (2/(Nsuper*Nsuper));
 disp('Theta = ' + Theta)
 zMax = 6; zMin = 0;%units Å
 
