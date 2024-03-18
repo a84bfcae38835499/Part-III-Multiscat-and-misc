11241881f335d101c2db33f7fe47717579dd6c91
UqUHXBs2VeA <oap22@cam.ac.uk>
2024-03-18 11:02:28 +0000

    test

--- potential_maker.m
+++ potential_maker.m
@@ -3,7 +3,7 @@ rng default;
 rng("shuffle");
 %Number of grid points, number of Z points, and number of lattices
 %contained in the overall superlattice (or rather the square root of that)
-Nxy = 32; Nz = 50; Nsuper = 3;
+Nxy = 32; Nz = 50; Nsuper = 2;
 %Theta = 0.0;
 Theta = (3/(Nsuper*Nsuper));
 disp('Theta = ' + Theta)
