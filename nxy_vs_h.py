e7f059094cf51de5d0c7e8c709158c312337ec3f
UqUHXBs2VeA <oap22@cam.ac.uk>
2024-03-19 14:11:58 +0000

    remove a load of autosave stuff

--- nxy_vs_h.py
+++ nxy_vs_h.py
@@ -1,22 +1,20 @@
 from matplotlib import pyplot as plt
+from matplotlib.ticker import MaxNLocator
 import numpy as np
-n1 = range(-3,0)
-intensities = np.array([0.360322E-01,0.520823E-01,0.138636E-01,0.811935E-01])
-
-#     -3     0          0.409479E-01
-#     -2     0          0.513849E-01
-#     -1     0          0.120501E-01
-#      0     0          0.724793E-01
-
-intensities_n = intensities/max(intensities)
-print(intensities_n)
+Nsuper = range(1,6)
+times_ms = np.array([6, 283, 2671, 10791, 50317])
+times_matlab = np.array([20, 50, 110, 193, 420])
 
 fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.hist(intensities_n,n1)
-plt.title("Intensity (normalised)")
-plt.ylim(0,1)
-plt.xlabel("n1")
-plt.xlim(-3,0)
-plt.ylabel("Entropy")
+ax1 = fig.add_subplot(111)
+plt.title("Time taken")
+ax1.set_ylim(6,51000)
+ax1.set_ylabel("Time taken /s")
+ax1.set_xlabel("Supercell size")
+ax1.set_xlim(1,5)
+ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
+ax1.set_yscale('log')
+ax1.plot(Nsuper,times_ms,label="Multiscat",color=[0., 0.5, 1.],linestyle=(0,(8,10)),marker='+',markersize=15)
+ax1.plot(Nsuper,times_matlab,label="MATLAB",color=[1., 0.5, 0.],linestyle=(0,(8,10)),marker='x',markersize=10)
+ax1.legend()
 plt.show()
