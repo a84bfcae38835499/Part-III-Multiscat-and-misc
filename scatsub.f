2555a43e56d323e2f4855e5447008bb817f85659
a84bfcae38835499 <oap22@cam.ac.uk>
2024-04-11 12:15:35 +0100

    resulkts

--- scatsub.f
+++ scatsub.f
@@ -387,7 +387,7 @@ c
       dimension d(n), e(m), f(m,n), p(n), t(m,m)
       dimension ix(n), iy(n), ivx(nfc), ivy(nfc)
       real startT, currT, dT
-      parameter (timeLimit = 30000) ! = 8 hours, 20 minutes
+      parameter (timeLimit = 43200) ! = 12 hours
 c
 c     NB:
 c     This subroutine implements a preconditioned version of GMRES(l).
@@ -413,11 +413,8 @@ c
       do i = 1,mn
          x(i) = (0.0d0,0.0d0) 
       enddo
-<<<<<<< Updated upstream
       
       call cpu_time(startT)
-=======
->>>>>>> Stashed changes
 c
 c     Initial step:
 c
@@ -564,6 +561,7 @@ c
          if (kconv.eq.3 .or. xnorm.eq.0.0d0) go to 2
          if(dT.gt.timeLimit) then
 139      format(A1,A,A,A1,A)
+            print *, ''
             write(*,139) achar(27),'[91m',
      +      ' ERROR: did not converger in time!',achar(27),'[0m'
             go to 2
